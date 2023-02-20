1e9b13a67e69b697c261f89562417690485d22bf
nagash87@gmail.com <nagash87@gmail.com>
2023-02-20 07:37:11 +0000

    fhv

---
+++ models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,32 @@
+{{ config(materialized='view') }}
+
+with tripdata as 
+(
+  select *, right(dispatching_base_num,5) as dispatching_base_num_int,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source('staging','fhv_tripdata') }}
+  where dispatching_base_num is not null 
+)
+select
+    -- identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num_int', 'pickup_datetime']) }} as tripid,
+    cast(dispatching_base_num_int as integer) as dispatching_base_num,
+    cast(PULocationID as integer) as PULocationID,
+    cast(DOLocationID as integer) as DOLocationID,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    SR_Flag,
+    Affiliated_base_number
+from tripdata
+where rn = 1
+
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
